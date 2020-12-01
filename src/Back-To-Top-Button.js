c54630a4d8ec37e3fe9473a478b1858fc1a17802
Super Panama World <superpanamaworld@gmail.com>
2020-12-01 02:38:14 -0800

    Add back to top button to bottom right
    - Uses little svg icon
    - Hidden on small screens since it takes up space

---
+++ src/Back-To-Top-Button.js
@@ -0,0 +1,21 @@
+import { useEffect, useRef } from 'preact/hooks'
+import './Back-To-Top-Button.css'
+
+export default function BackToTopButton () {
+  const button = useRef(null)
+  useEffect(() => {
+    button.current.addEventListener('click', (e) => {
+      console.log(e)
+      window.scroll(0, 0)
+    })
+  }, [])
+  return (
+    <div className={'back-to-top-button'} role="button" ref={button}>
+      <div className={'back-to-top-button-arrow-container'}>
+        <svg width="50" height="50" xmlns="http://www.w3.org/2000/svg" version="1.1">
+          <polyline points="10 50 25 25 40 50 25 40 10 50" strokeLinecap="butt" fill="#ccccccaa" strokeLinejoin="miter"/>
+        </svg>
+      </div>
+    </div>
+  )
+}
