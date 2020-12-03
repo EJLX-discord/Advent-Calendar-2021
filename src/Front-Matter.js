ccddf2662b19a1047643ea55b14eebb6fa2d6e37
Super Panama World <superpanamaworld@gmail.com>
2020-12-02 20:29:40 -0800

    Add a link in upper right back to AC Hub

--- src/Front-Matter.js
+++ src/Front-Matter.js
@@ -13,6 +13,19 @@ export default function FrontMatter () {
       <Wave color={'#046e8f'} height={-100 + height / 4 * 3} priority={4}/>
       <Wave color={'#183446'} height={-100 + height / 4 * 4} priority={5}/>
       <div className={'front-page'} style={{ height }}>
+        <a
+          className={'front-page-go-back-link'}
+          href={'https://espaspw.github.io/Advent-Calendar-Hub/'}
+        >
+          <span style={{
+            position: 'relative',
+            bottom: '1px',
+            marginRight: '5px'
+          }}>
+            {'<'}
+          </span>
+          See Other Years
+        </a>
         <h1 className={'front-page-title'} style={{ top: height / 6 }}>
           EJLX Advent Calendar 2020
         </h1>
