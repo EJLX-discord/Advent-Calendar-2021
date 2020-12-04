4fe2d4ac68a664a6bff6a79286c2a69047ae2b14
Super Panama World <superpanamaworld@gmail.com>
2020-12-03 20:50:52 -0800

    Add ayumminu's entry

--- src/data/entries/4.js
+++ src/data/entries/4.js
@@ -1,7 +1,63 @@
 import icon from '../icons/ayumminu.png'
 
 export default (
-  <></>
+  <>
+    <div>Advent Calendar: the 4th of December 2020. </div>
+    <div className={'entry-block'}>
+      <p className={'entry-block-header'}>INT. A chat room - NIGHT</p>
+      <p className={'unemph'}>(Inu and Neco are chatting on DM. Their name is username. They don&apos;t know each other&apos;s real names.）</p>
+      <p>Inu(Ayuwoof): It was great to chat tonight. Thank you and see you next week. Good night Neco.</p>
+      <p>Neco (Ayumeow): Thank you Inu. Good night! :)</p>
+    </div>
+    <div className={'entry-block'}>
+      <p className={'entry-block-header'}>INT. Ayumeow&apos;s room - NIGHT</p>
+      <p className={'unemph'}>(There are beauty goods in her room. Ayumeow turns off her laptop computer)</p>
+      <p>Ayumeow: Ohhh, my gosh! It was so much fun! I love Inu! Ah, I wish this kind of time could go on forever.</p>
+      <p className={'unemph'}>(Ayumeow embarrassed, covering her face with her hands)</p>
+    </div>
+    <div className={'entry-block'}>
+      <p className={'entry-block-header'}>INT. Ayuwoof&apos;s room - NIGHT</p>
+      <p className={'unemph'}>(Ayuwoof turns off his laptop computer. He sighs deeply.)</p>
+      <p>Ayuwoof: It's more about Ayumeow than chatting. I&apos;m sure she would have forgotten about me. It was just a year ago today that I was helped by you. Do you know that I'm finally a dog cop? </p>
+      <p className={'unemph'}>(Ayuwoof looks at a poster of the nation&apos;s most popular pop-cat-idol Ayumeow)</p>
+    </div>
+    <div className={'entry-block'}>
+      <p className={'entry-block-header'}>INT. Angela&apos;s room - DAY</p>
+      <p className={'unemph'}>(She was dumped by her boyfriend via a chat a few minutes ago in this morning. With the phone in her hand, Angela was crying on her bed.)</p>
+      <p>Angela: Why? What made him angry? I don&apos;t know him at all! </p>
+      <p className={'unemph'}>(She hears a mysterious voice, something whispering directly into her head.)</p>
+      <p> Shinigami: I will give you the superpower of darkness.</p>
+      <p> Angela: What!? Who are you? </p>
+      <p> Shinigami: I&apos;m Shinigami, I&apos;m on your side. With this power, you can take away the good looks of pop-cat-idol Ayumeow and money of a dog cop Ayuwoof. </p>
+      <p> Angela: I want to get the beauty and money to revenge on my ex!</p>
+      <p className={'unemph'}> (Shinigami makes that a darkness apple appears in front of Angela.)</p>
+      <p>Shinigami: Take a bite of this special apple. You can turn your heartbreak into anger.</p>
+      <p className={'unemph'}>(Angela eats its apple. She was transformed into &quot;Angera&quot; by Shinigami&apos;s dark power. She fearless smiling, saying)</p>
+      <p>Angera: Anger is justice! I defeat those two and take it all!</p>
+    </div>
+    <div className={'entry-block'}>
+      <p className={'entry-block-header'}>EXT/INT. Ayumeow&apos;s live hall  - DAY</p>
+      <p className={'unemph'}>(Angera: looks for Ayumeow, and smashing the Christmas decorations on this hall in anger. Ayuwoof comes to Ayumeow)</p>
+      <p>Ayuwoof: Hello, my sweet. Missed me?</p>
+      <p className={'unemph'}>(Ayuwoof, wearing a policeman&apos;s uniform, approaches Ayumeow with a bouquet of roses)</p>
+      <p>Ayumeow: Ew disgusting! I have someone to love not you.</p>
+      <p>Ayuwoof:  I see. By the way, we don&apos;t know each other&apos;s yet, do we? What are your hobbies? What&apos;s your favourite thing? I want to know about you more.</p>
+      <p>Ayumeow: My hobby is watching movies. Wait, why do I have to say it now? We have to defeat that angry monster.</p>
+      <p className={'unemph'}>(Ayumeow looks at Ayuwoof with a dumbfounded expression.)</p>
+      <p>Ayuwoof: Hahaha. I want you to remember me. My hobby is drawing. I would like to draw you a picture next time. </p>
+      <p>Ayumeow: No thank you.</p>
+      <p className={'unemph'}>(Angera finds Ayumeow and Ayuwoof having a conversation and approaches. They noticed her approaching.)</p>
+      <p>Ayuwoof: Oh, I forgot to say!</p>
+      <p className={'unemph'}>(Ayuwoof says one word to Ayumeow, something. Ayuwoof goes to take down Angera alone.)</p>
+      <p>Ayumeow: Why......</p>
+      <p className={'unemph'}>(A single tear on her cheek)</p>
+    </div>
+    <div className={'entry-block'}>
+      <p>------</p>
+      <p>my comment:  I&apos;ve written the post contents of the three people who have posted AC so far, and a part of my name.
+      Thank you for taking your time to read my article. Please don&apos;t hesitate to send me your corrections via DM.</p>
+    </div>
+  </>
 )
 
 export const metadata = {
