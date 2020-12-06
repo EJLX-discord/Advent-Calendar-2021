efdad3287472b912409a6cdae415a902e52d3d23
Super Panama World <superpanamaworld@gmail.com>
2020-12-06 15:21:04 -0800

    Add entries 5,6,7

--- src/data/entries/5.js
+++ src/data/entries/5.js
@@ -1,7 +1,26 @@
 import icon from '../icons/メーヴェ.png'
 
 export default (
-  <></>
+  <>
+    <div>December 5th, 2020</div>
+    <div>
+      <p>Merry Christmas!<br/>At first, I&apos;m hoping you guys are having great holidays.</p>
+      <p>Lately I&apos;m having a lot of to do in my life. like, my gramma got dementia, also my job became really really busy for some reasons, even though I speak English every days with some of my friends on the VC.</p>
+      <p>My next year&apos;s resolutions are</p>
+      <ol>
+        <li>Speak/listen in English better than now.</li>
+        <li>Learn English way more accurate in some Univ.</li>
+        <li>Take the license, called &quot;1st class Qualified Certified Electrician&quot;.</li>
+        <li>Cherish my precious friend, also family, and gramma.</li>
+      </ol>
+      <p>3 of those resolutions were related by the COVID sadly...<br/>
+      But... I mean...</p>
+      <p>Everything definitely will be fine.<br/>At this moment, the entire world on fire. But STAY STRONG EVERYONE!!!!</p>
+      <p>After this COVID became better, let&apos;s hangout like good old days, hm??</p>
+      <p>Please take care for your healthy life, and make your year the best one next year!! friends!</p>
+      <p>Happy new year!!!!</p>
+    </div>
+  </>
 )
 
 export const metadata = {
--- src/data/entries/6.js
+++ src/data/entries/6.js
@@ -1,7 +1,16 @@
 import icon from '../icons/daku.gif'
 
 export default (
-  <></>
+  <>
+  <div>December 6th, 2020</div>
+  <div className={'jp'}>
+    <p>
+      僕は桜木。僕もJon.僕も黄修心です。私は英語を教える、大好き。きょう、僕の日本語はまずいでも私は頑張ります。来年は良くなります。明日は良くなります。今日は最善を尽くすことが重要です。
+      はじめまして、皆さん、よろしくおねがいします。あなたはすべて私の大切な友達です。
+    </p>
+  </div>
+  <p>Happy new year and Merry Christmas)</p>
+  </>
 )
 
 export const metadata = {
