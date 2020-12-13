09f523fa4379ccab32ddeda7e3d728ffe12b15c3
Super Panama World <superpanamaworld@gmail.com>
2020-12-13 13:33:01 -0800

    Add entry 13

--- src/data/entries/13.js
+++ src/data/entries/13.js
@@ -1,7 +1,30 @@
 import icon from '../icons/simp.gif'
 
+import agBow from '../emotes/agBow.gif'
+import rooSantaAww from '../emotes/rooSantaAww.png'
+import prettyThumbsUp from '../emotes/prettyThumbsUp.png'
+import yotsubaUwah from '../emotes/yotsubaUwah.png'
+
 export default (
-  <></>
+  <>
+    <div>
+    <strong><em>Submission for Advent Calendar</em></strong>
+    </div>
+    <div className={'jp'}>
+      <p>今日は２０２０年１２月１３日です。December 13, 2020</p>
+
+      <p>こんにちは｡私名前はマツオです。名前もヤオです。本日は、私のアドベントカレンダー日です！よろしくお願いします。<img src={agBow} className={'emote'} /> </p>
+
+      <p>毎日は、このサーバーを見ると、日本語が頑張って勉強します。でも、勉強はむずかしいですね？やっぱり、私はフィリピン人が日本語は下手でしょう（笑）。だから、来年でN5のテストをしてます。多分、N4のテストできるかな…</p>
+
+      <p>私のホービはライトノベルを読みます｡ホービもゲームをします。ライトノベルで「いせかい」と「空想」は大好き〜です。ときどき、「ミステリー」は大好きを読みます。私が大好きなものは、ライトノベルは「オバーロード」と「ザ．ニュー．ゲート」です。新年の抱負は、N5かN4なります<img src={rooSantaAww} className={'emote'} /> </p>
+
+      <p>これははずかしい、でも皆さんは楽しんでください<img src={prettyThumbsUp} className={'emote'} /></p>
+    </div>
+    <div>
+      <p>By Matsuo, whose Japanese is really really bad<img src={yotsubaUwah} className={'emote'} /></p>
+    </div>
+  </>
 )
 
 export const metadata = {
