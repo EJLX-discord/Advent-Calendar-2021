05ab0c794f18d12b1871b2b3d8e86b7b971da29e
Super Panama World <superpanamaworld@gmail.com>
2020-12-29 13:33:58 -0800

    Add entry 29

--- src/data/entries/29.js
+++ src/data/entries/29.js
@@ -1,6 +1,31 @@
 import icon from '../icons/morisi.png'
+import pekoraSalute from '../emotes/pekoraSalute.png'
+
 export default (
-  <></>
+  <>
+  <div>
+    Advent Calendar 2020 - December 29 (Tue)
+  </div>
+
+  <div>
+    <p><strong>The Rise of Vtubers</strong></p>
+    <p>
+      Have you ever heard of Virtual Youtubers (Vtubers)? They broadcast games, songs or free talks mainly on YouTube with anime looking avatars. According to statistics, there are already more than 13,000 Vtubers as of November 2020. Some of them have earned around 100 million yen per year solely by &quot;Super Chats&quot;,  monetary donations that let viewers emphasize their messages to draw attention from the Vtubers to have them say &quot; Thank you.&quot; Once Vtubers have successfully built a solid community, they can share their supporters by collaborating with others in the same group.  For Hololive, one of the largest Japanese Vtuber groups, 2020 was a resounding success. Their strategy is quite similar to the existing Japanese idol group management scheme known as <span className={'jp'}>箱推し（はこおし）</span>. They encourage their Vtubers to broadcast with other members to expand their fan base. Besides that, Super chats worked very well with the Japanese style to enjoy the process of developing idols, which is different from Korean idols where only a few people can become idols through strict selections after going to dance school.
+    </p>
+    <p>
+      As it happened to everyone in the world, COVID-19 has completely changed my lifestyle. The government requested us to stay home as much as we could. We feel hesitant to invite people to hang out with somewhere outside in this situation. I am not a huge fan of party, so I would rather appreciate that I do not have to go out to drink hard now. However, as I come to spend most of the time at home by myself for a long period, I sometimes feel lonely because of a lack of communication. One day, when I asked my friends about how they deal with this situation, they told me that there were anime girls talking random/everyday stuff with cute voices and they are perfect when working from home since they fill the void of loneliness. Although they suggested two Vtubers from Hololives, I did not watch YouTube at that time, and I was not interested in the anime-ish girls either, so I made light of them at first. A few days later, I felt like watching Vtubers for some reason. I tried a girl with bunny ears who had a weird speech style, peko. I was fascinated with her unique voice and reactions when playing challenging games. As I did not expect that she would yell at the game and show frustration straightforwardly with that cute face, I found the unmatch funny. Then I started to watch other Vtubers as well, which revived my interest in Apex Legends since it is quite popular among the ones from Ichikara (another Vtuber management company).
+    </p>
+    <p>
+      I am not watching Vtubers as much as 3 months ago since I am busy studying English and other stuff and practising instruments for my band that I started. In the meantime, the Vtubers I was watching have doubled the number of subscribers, compared to 6 months ago. Hololive has English-speaking members, so if you are learning English, you can justify watching them playing among us as studying, not procrastinating. For Japanese learners, of course, they are good learning material because they are talking about various things in natural speech, except for some members (peko). If you are interested, watching short clips made by fans would help you find your favorite streamer. Also, because they usually have either Japanese or English subs, you do not have to be worried about your listening skills.
+    </p>
+
+    <p>
+      Lastly, thank you very much for reading.<br/>
+      Wish you all Happy Holidays...peko!
+    </p>
+    <img src={pekoraSalute} className={'emote-large'} />
+  </div>
+  </>
 )
 
 export const metadata = {
