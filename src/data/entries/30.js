0bf8cd08f3ba89881b6bdfbbeceb13235134cc69
Super Panama World <superpanamaworld@gmail.com>
2020-12-29 21:01:38 -0800

    Add entry 30

--- src/data/entries/30.js
+++ src/data/entries/30.js
@@ -1,7 +1,35 @@
 import icon from '../icons/taku.png'
+import image from '../images/30.jpg'
 
 export default (
-  <></>
+  <>
+    <div>
+      Advent Calendar 30th December 2020
+    </div>
+    <div>
+      <p>First of all, I am very happy to join the Advent Calendar .<br/>
+      I got an idea of a story when I received one picture from my friend Daku. It was an image of his cat. His name is Ruka. </p>
+      <br/>
+      <p>『  Hear me out ！ 』</p>
+
+      <p>Ruka🐱  &quot; There&apos;s something I want to ask you..... It seems that there are really delicious foods in Japan that I can lick and eat . I was wondering if you can buy it here in America ,too.  But I would really love to go to Japan one day .<br/>
+      I know some Japanese <br/>
+      「 SUSHI 」 「 TEMPURA 」 「 PARIDON 」「 KAWAII 」  「EMOI 」  「 PIENN 」</p>
+
+      <p>Daku 🧒  &quot;Huh？  when did you learn such Japanese？&quot;</p>
+
+      <p>Ruka🐱   &quot;I &apos;ve been watching you study Japanese everyday beside me.  So you don&apos;t have to be so surprised . But I know that you don&apos;t get anything of what I mean when you hear me just say <br/>
+      【 meow】</p>
+
+      <img src={image} alt={'A beautiful cat on a wooden floor'} className={'entry-image'} />
+
+      <p>Thank you very much to read my story.<br/>
+      And<br/>
+      Yoi otoshi wo .<br/>
+      You may be happy new year.</p>
+
+    </div>
+  </>
 )
 
 export const metadata = {
