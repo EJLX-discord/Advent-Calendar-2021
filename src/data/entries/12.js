33676b52920e8719aaa1cc58c013a2801f1162b6
Dmitry Ponkin <dmi.ponkin@gmail.com>
2022-11-25 20:28:28 +0900

    add 11 and 12

---
+++ src/data/entries/12.js
@@ -0,0 +1,24 @@
+import icon from '../icons/zabela.webp'
+import audio from '../audio/oaxith.mp3'
+
+export default (
+  <>
+  <div className={'jp'}>
+    <strong>2021 アドベントカレンダー 12</strong>
+  </div>
+  <div className={'jp'}>
+    <p>私のエントリーは録音になります！ぜひ聞いて下さい！（ヘッドフォンを推奨します）<br />
+    録音とスクリプトに相違が出たので、内容を合わせてからcorrect_meの方で投稿します。</p>
+  </div>
+    audio
+  </>
+)
+
+export const metadata = {
+  alias: 'oaxith',
+  username: 'oaxith#9001',
+  id: '185051004763504640',
+  index: 12,
+  date: 'December 12',
+  icon: icon
+}
--- src/index.js
+++ src/index.js
@@ -83,10 +83,13 @@ export default function App () {
   const cookieVal = cookies.get('isSnowEnabled')
   const firstState = cookieVal === undefined ? true : cookieVal === 'true'
   const [isSnowEnabled, setIsSnowEnabled] = useState(firstState)
+  // const entries = getEntries([
+  //   1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+  //   11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  //   21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
+  // ])
   const entries = getEntries([
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-    11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-    21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10
   ])
 
   useEffect(() => {
